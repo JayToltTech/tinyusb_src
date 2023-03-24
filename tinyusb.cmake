34e4d8e902a7d09bb9684d82d144e058df8e43fc
Jay Beavers <jay@tolttechnologies.com>
2023-03-23 21:25:22 -0700

    Add ncm_device.c to list of src files

--- tinyusb.cmake
+++ tinyusb.cmake
@@ -1,10 +1,10 @@
 target_sources(${PROJECT_NAME} PRIVATE
-
   ${CMAKE_CURRENT_LIST_DIR}/src/portable/microchip/samd/dcd_samd.c
   ${CMAKE_CURRENT_LIST_DIR}/src/tusb.c
   ${CMAKE_CURRENT_LIST_DIR}/src/common/tusb_fifo.c
   ${CMAKE_CURRENT_LIST_DIR}/src/device/usbd_control.c
   ${CMAKE_CURRENT_LIST_DIR}/src/device/usbd.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/class/net/ncm_device.c
 )
 
 target_include_directories(${PROJECT_NAME} PRIVATE
