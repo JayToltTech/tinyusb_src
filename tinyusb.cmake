04f985b5e3da35d5c6ef263b523f586b94b11863
Jay Beavers <jay@tolttechnologies.com>
2023-04-19 09:29:30 -0700

    Remove unused source files

--- tinyusb.cmake
+++ tinyusb.cmake
@@ -1,9 +1,6 @@
 target_sources(${PROJECT_NAME} PRIVATE
   ${CMAKE_CURRENT_LIST_DIR}/src/tusb.c
-  ${CMAKE_CURRENT_LIST_DIR}/src/class/cdc/cdc_device.c  // Not sure this is needed
   ${CMAKE_CURRENT_LIST_DIR}/src/class/net/ncm_device.c
-  ${CMAKE_CURRENT_LIST_DIR}/src/class/vendor/vendor_device.c  // Not sure this is needed
-  ${CMAKE_CURRENT_LIST_DIR}/src/common/tusb_fifo.c
   ${CMAKE_CURRENT_LIST_DIR}/src/device/usbd_control.c
   ${CMAKE_CURRENT_LIST_DIR}/src/device/usbd.c
   ${CMAKE_CURRENT_LIST_DIR}/src/portable/microchip/samd/dcd_samd.c
